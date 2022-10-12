95570f2a8d8b1e37459cf129620d1ba4935ca1af
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-13 01:29:42 +0530

    refactor:changes in condition

--- app.js
+++ app.js
@@ -4,35 +4,38 @@ var cprice=document.querySelector("#currentprice");
 var check=document.querySelector("#buto");
 var writetext=document.querySelector(".textmessage")
 function calculateProfitloss(initial,snumber,current)
-{ writetext.style.display="none"
+{ 
+    
+if((initial!=="")&&(snumber!=="")&&(current!==""))
+{    writetext.style.display="none"
     if(Number(initial)>Number(current))
-    {
-        writetext.style.display="block"
+    { writetext.style.display="block"
         var loss=Number(initial)-Number(current)
         var losspercentage=(loss*100)/initial
         writetext.innerText= "Loss is"+" "+loss+"& loss percentage is"+" "+losspercentage;
     } 
     else if (Number(initial)<Number(current))
-    { writetext.style.display="block"
+    {  writetext.style.display="block"
         var profit=(Number(current)-Number(initial))*snumber
         var profitpercentage=(profit*100)/initial
         writetext.innerText= "profit is"+" "+profit+"& profit percentage is"+" "+profitpercentage
 
     }
     else
-{
-    if(initial==""||snumber==""||current=="")
-{
-    writetext.style.display="block"
-    writetext.innerText="Enter the values";
-}
-else{
-  writetext.style.display="block"
+    { writetext.style.display="block"
+  
         writetext.innerText="NO pain No gain.";
- }
- }
+  }
+   
+}
+else
+{
+    writetext.innerText="Enter the values";
 
 }
+    
 
+}
+writetext.innerText="Enter the values";
 check.addEventListener("click",calculateProfitloss((tprice.value),(ns.value),(cprice.value)))
 
